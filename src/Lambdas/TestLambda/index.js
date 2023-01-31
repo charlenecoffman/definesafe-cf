306589a7f3b15e442ebf6c9e62c3fa9aa49734d3
Charlene Coffman <charlene.coffman@roofstock.com>
2023-01-31 14:23:01 -0600

    remove test, updating sleep for lambda

--- src/Lambdas/TestLambda/index.js
+++
@@ -1,7 +0,0 @@
-var aws = require('aws-sdk');
-
-exports.handler = async (event, context, callback) => {
-    const name = event.name || 'World';
-    const response = {greeting: `Hello, ${name}!`};
-    callback(null, response);
-}
