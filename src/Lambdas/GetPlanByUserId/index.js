689619118f7b346f8ef5dea3aa2f38f76566d9c8
Charlene Coffman <charlene.coffman@roofstock.com>
2023-02-01 12:59:21 -0600

    ok try this

--- src/Lambdas/GetPlanByUserId/index.js
+++ src/Lambdas/GetPlanByUserId/index.js
@@ -3,6 +3,12 @@ var aws = require('aws-sdk');
 exports.handler = async (event, context, callback) => {
     const user_id_from_event_dot = event.user_id;
     const user_id_from_event_with_index = event["queryStringParameters"]["user_id"]
-    const response = {"something": [user_id_from_event_dot, user_id_from_event_with_index]};
+    const response ={
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "*/*"
+        },
+        "body": {"something": [user_id_from_event_dot, user_id_from_event_with_index]}
+    };
     callback(null, response);
 }
