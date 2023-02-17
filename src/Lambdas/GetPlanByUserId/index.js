528b1e945d0487d01c199fa4f079b9c609e72411
Charlene Coffman <charlene.coffman@roofstock.com>
2023-02-17 10:34:21 -0600

    trying to create global index, plus update the get

--- src/Lambdas/GetPlanByUserId/index.js
+++ src/Lambdas/GetPlanByUserId/index.js
@@ -1,14 +1,31 @@
 var aws = require('aws-sdk');
+var documentClient = new aws.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context, callback) => {
-    const user_id = event["queryStringParameters"]["user_id"]
-
-
     const response ={
-        "isBase64Encoded": false,
         "statusCode": 200,
-        "headers": { "Content-Type": "*/*" },
-        "body": {"something": user_id}
+        "headers": {
+            "Content-Type": "*/*"
+        }
     };
-    callback(null, response);
+
+    const qsParams = JSON.parse(event.queryStringParameters );
+
+    var params = {
+      TableName: 'Plans',
+      Key: {
+        'User_Id':  qsParams.User_Id
+      }
+    };
+
+    await documentClient.get(params)
+      .promise()
+      .then(resp => {
+        response.body = JSON.stringify(params);
+        callback(null, response);
+      })
+      .catch(err => {
+        response.body = JSON.stringify(err);
+        callback(null, response)
+      });
 }
