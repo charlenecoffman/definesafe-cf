ef0ffca75d20166eaee10cd1a93706cca0a7ebe0
Charlene Coffman <charlene.coffman@roofstock.com>
2023-02-17 20:39:04 -0600

    iono

--- src/Lambdas/GetPlanByUserId/index.js
+++ src/Lambdas/GetPlanByUserId/index.js
@@ -18,6 +18,7 @@ exports.handler = async (event, context, callback) => {
       ExpressionAttributeValues: {
           ":User_Id": qsParams.User_Id
       },
+      Select: ALL_ATTRIBUTES
     };
 
     await documentClient.query(params)
