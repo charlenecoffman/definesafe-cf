ea83b34a0cc4a49060fb98f46b491a2700d546c4
Charlene Coffman <charlene.coffman@roofstock.com>
2023-04-05 18:41:32 -0500

    hah we will see

--- src/Lambdas/GetAllPlans/index.js
+++ src/Lambdas/GetAllPlans/index.js
@@ -1,5 +1,6 @@
 var aws = require('aws-sdk');
 var documentClient = new aws.DynamoDB.DocumentClient();
+const jwt_decode = require('jwt-decode');
 
 exports.handler = async (event, context, callback) => {
     const response ={
@@ -12,6 +13,10 @@ exports.handler = async (event, context, callback) => {
     var params = {
       TableName: 'Plans'
     };
+
+    console.log(event.headers)
+
+    var decoded = jwt_decode(event.headers["Authorization"]);
 
     await documentClient.scan(params)
       .promise()
