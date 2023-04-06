d53b7e0becd8d5bc4d9b84ad4e1c4f20529528d1
Charlene Coffman <charlene.coffman@roofstock.com>
2023-04-06 09:25:13 -0500

    okay whatevs

--- src/Lambdas/GetAllPlans/index.js
+++ src/Lambdas/GetAllPlans/index.js
@@ -1,25 +1,43 @@
 var aws = require('aws-sdk');
 var documentClient = new aws.DynamoDB.DocumentClient();
 const jwt_decode = require('jwt-decode');
+var axios = require("axios").default;
 
 exports.handler = async (event, context, callback) => {
-    const response ={
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "*/*"
-        }
+
+
+    const decoded = jwt_decode(event.headers["Authorization"]);
+    const user_id = decoded.sub.split("@")[0];
+
+    var options = {
+      method: 'POST',
+      url: process.env.URL,
+      headers: {'content-type': 'application/x-www-form-urlencoded'},
+      data: new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: '{yourClientSecret}',
+        audience: process.env.AUDIENCE
+      })
     };
     
-    var params = {
+    axios.request(options).then(function (response) {
+      
+    }).catch(function (error) {
+      console.error(error);
+    });
+
+    const response ={
+      "statusCode": 200,
+      "headers": {
+          "Content-Type": "*/*"
+      }
+    };
+  
+    const params = {
       TableName: 'Plans'
     };
-
-    console.log(event.headers);
-
-    var decoded = jwt_decode(event.headers["Authorization"]);
-    var user_id = decoded.sub.split("@")[0];
-    console.log(user_id);
-
+    
     await documentClient.scan(params)
       .promise()
       .then(resp => {
