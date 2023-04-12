1dee647c4f14918c1025d658fb2dffe845ffe816
Charlene Coffman <CCoffman@ebsco.com>
2023-04-12 08:18:04 -0500

    working on confirming user stuff 5

--- src/Lambdas/GetAllPlans/index.js
+++ src/Lambdas/GetAllPlans/index.js
@@ -23,7 +23,7 @@ exports.handler = async (event, context, callback) => {
     
     console.log(options1);
     var adminToken = await axios.request(options1);
-
+    console.log(adminToken);
     const decoded = jwt_decode(event.headers["Authorization"]);
     const user_id = decoded.sub;
 
