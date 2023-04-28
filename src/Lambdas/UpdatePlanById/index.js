e4c7c098502a65b209779195034948b3535bae6d
Charlene Coffman <CCoffman@ebsco.com>
2023-04-28 10:55:29 -0500

    another try

--- src/Lambdas/UpdatePlanById/index.js
+++ src/Lambdas/UpdatePlanById/index.js
@@ -3,72 +3,68 @@ var documentClient = new aws.DynamoDB.DocumentClient();
 const jwt_decode = require('jwt-decode');
 
 exports.handler = async (event, context, callback) => {
-    const response ={
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "*/*"
-        }
-    };
 
+    const response = InitialResponseObject();
     const updatePlan = JSON.parse(event.body);
+    const user_id = GetUserIdFromHeaders(event);
 
-    const decoded = jwt_decode(event.headers["Authorization"]);
-    const user_id = decoded.sub;
-
-    var getPlanParams = {
-      TableName: 'Plans',
-      IndexName: 'User_Id',
-      KeyConditionExpression: "User_Id = :User_Id",
-      ExpressionAttributeValues: {
-          ":User_Id": user_id
-      },
-      Select: 'ALL_ATTRIBUTES'
-    };
-    someFunction();
-    await documentClient.query(getPlanParams)
-      .promise()
-      .then(resp => {
-        console.log(resp);
-        if(resp.Plan_Id !== updatePlan.Plan_Id){
-          response.statusCode = 401;
-          response.body = "This user is not allowed to execute this update";
-          callback(null, response)
-        }
-      })
-      .catch(err => {
-        response.statusCode = 500;
-        response.body = JSON.stringify(err);
-        callback(null, response)
-      });
-
-    var params = {
-      TableName: 'Plans',
-      Key: {
-        Plan_Id: updatePlan.Plan_Id
-      },
-      UpdateExpression: 'SET Triggers = :triggers, Coping_Skills = :copingSkills',
-      ConditionExpression: 'attribute_exists(Plan_Id)',
-      ExpressionAttributeValues: {
-        ':triggers' : updatePlan.Triggers,
-        ':copingSkills' : updatePlan.Coping_Skills,
-      }
-    };
-
-    await documentClient.update(params)
-      .promise()
-      .then(resp => {
-        console.log(resp);
-        response.body = JSON.stringify(updatePlan);
-        callback(null, response);
-      })
-      .catch(err => {
-        response.body = JSON.stringify(err);
-        response.body += JSON.stringify(params);
-        callback(null, response)
-      });
+    const getPlanParams = GetQueryParams(user_id);
+    const updatePlanParams = GetUpdateParams(updatePlan);
     
+    const queryData = await documentClient.query(getPlanParams).promise();
+
+    if(queryData.Items[0].Plan_Id != updatePlan.Plan_Id)
+    {
+        response.statusCode = 401;
+        response.body = "This user is not allowed to execute this update";
+        callback(null, response)
+    }
+
+    const updateResponse = await documentClient.update(updatePlanParams).promise();
+    
+    console.log(updateResponse);
+    
+    response.body = JSON.stringify(updateResponse);
+    callback(null, response);
 }
 
-const someFunction = () => {
-  console.log("testing 1234")
+const InitialResponseObject = () => {
+  return {
+    "statusCode": 200,
+    "headers": {
+        "Content-Type": "*/*"
+    }
+  };
+}
+
+const GetUserIdFromHeaders = (event) => {
+  const decoded = jwt_decode(event.headers["Authorization"]);
+  return decoded.sub;
+}
+
+const GetQueryParams = (user_id) => {
+  return {
+    TableName: 'Plans',
+    IndexName: 'User_Id',
+    KeyConditionExpression: "User_Id = :User_Id",
+    ExpressionAttributeValues: {
+        ":User_Id": user_id
+    },
+    Select: 'ALL_ATTRIBUTES'
+  };
+}
+
+const GetUpdateParams = (updatePlan) => {
+  return {
+    TableName: 'Plans',
+    Key: {
+      Plan_Id: updatePlan.Plan_Id
+    },
+    UpdateExpression: 'SET Triggers = :triggers, Coping_Skills = :copingSkills',
+    ConditionExpression: 'attribute_exists(Plan_Id)',
+    ExpressionAttributeValues: {
+      ':triggers' : updatePlan.Triggers,
+      ':copingSkills' : updatePlan.Coping_Skills,
+    }
+  };
 }
